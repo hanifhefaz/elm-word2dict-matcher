4985add0353feb4d8a4b39bc7a131478df27c4d5
hanifhefaz <hefaz2010@gmail.com>
2021-04-23 01:21:10 +0200

    UI Optimization

--- examples/ChatExample/src/Pages/Chat.elm
+++ examples/ChatExample/src/Pages/Chat.elm
@@ -1,17 +1,16 @@
-module Pages.Chat exposing (Params,Model,Msg,page)
+module Pages.Chat exposing (Model, Msg, Params, page)
 
-import Spa.Document exposing (Document)
-import Spa.Url as Url exposing (Url)
 import Browser exposing (sandbox)
 import Dict exposing (..)
-import Spa.Page as Page exposing (Page)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput, onSubmit)
-import Html
-import Word2DictMatcher exposing (..)
 import QuestionsBank
-
+import Spa.Document exposing (Document)
+import Spa.Page as Page exposing (Page)
+import Spa.Url as Url exposing (Url)
+import String exposing (append)
+import Word2DictMatcher exposing (..)
 
 
 page : Page Params Model Msg
@@ -23,10 +22,30 @@ page =
         }
 
 
+type alias Message =
+    { participant : Participant, message : String }
+
+
 type alias Model =
-    { searchString : String
-    , searchResult : SearchResult
+    { messages : List Message
+    , message : String
     }
+
+
+type Participant
+    = Human
+    | Bot
+
+
+type Msg
+    = TypedMessage String
+    | PostedMessage String
+
+
+type SearchResult
+    = NotSearched
+    | Searched (Maybe QuestionsBank.Answer)
+
 
 type alias Params =
     ()
@@ -34,59 +53,84 @@ type alias Params =
 
 init : Url Params -> Model
 init { params } =
-    {searchString = ""
-    , searchResult = NotSearched
+    { messages = []
+    , message = ""
     }
 
-type Msg
-    = TypedSearch String
-    | Search
-
-type SearchResult
-    = NotSearched
-    | Searched (Maybe QuestionsBank.Answer)
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        TypedSearch str ->
-            { model | searchString = str }
+        TypedMessage str ->
+            { model | message = str }
 
-        Search ->
-            { model | searchResult = Searched <| Word2DictMatcher.findRelevantDict (model.searchString |> Word2DictMatcher.tokenize |> Word2DictMatcher.toHistogram) QuestionsBank.data }
+        PostedMessage "" ->
+            { model | message = "" }
+
+        PostedMessage str ->
+            let
+                answer =
+                    Searched <| Word2DictMatcher.findRelevantDict (str |> Word2DictMatcher.tokenize |> Word2DictMatcher.toHistogram) QuestionsBank.data
+
+                answerMessage =
+                    case answer of
+                        Searched a ->
+                            case a of
+                                Just ans ->
+                                    ans.answer
+
+                                _ ->
+                                    ""
+
+                        _ ->
+                            ""
+            in
+            { model
+                | messages =
+                    { participant = Bot, message = answerMessage }
+                        :: { participant = Human, message = str }
+                        :: model.messages
+                , message = ""
+            }
+
+
+viewMessage : Message -> Html Msg
+viewMessage { participant, message } =
+    case participant of
+        Human ->
+            div [ style "background-color" "#0B62B8", style "padding" "5px", style "color" "white", style "font-size" "18px" ] [ i [] [ text message ] ]
+
+        Bot ->
+            div [ style "background-color" "#F1F2F3", style "padding" "5px", style "font-size" "15px" ] [ i [] [ text message ] ]
 
 
 view : Model -> Document Msg
 view model =
     { title = "Main"
-    , body = [div [ class "row py-5" ]
+    , body =
+        [ div [ class "row py-5" ]
             [ div [ class "col-lg-12 mx-auto" ]
                 [ div [ class "text-white p-5 shadow-sm rounded banner" ]
                     [ h1 [ class "display-4" ]
-                        [ text "Ask our Agent" ]
+                        [ text "Ask our Agent Anything" ]
                     , p [ class "lead" ]
                         [ text "Do you have any question, regarding our products?" ]
-                        , div [ style "text-align" "center" ]
-        [ Html.form[ onSubmit Search ][
-         textarea [ style "width" "50%",style "margin" "auto", style "height" "150px", style "padding" "10px", onInput TypedSearch, value model.searchString ] []
-        , br [][]
-        , br [][]
-        , button [class "btn btn-dark px-5 py-3 text-uppercase"] [ text "Ask me" ]]
-        , br [][]
-        , div [ style "width" "50%",style "background-color" "#32CD32",style "margin" "auto", style "height" "20%", style "padding" "40px" ]
-            [ case model.searchResult of
-                NotSearched ->
-                    text ""
-
-                Searched Nothing ->
-                    text "Sorry, there is no words in the bag :)"
-
-                Searched (Just answer) ->
-                    text answer.answer
-            ]
-        ]
                     ]
                 ]
             ]
+        , p [ style "margin" "auto", style "text-align" "center" ] [ text "pleas type your question here and press Enter" ]
+        , div [ style "width" "25%", style "margin" "auto", style "height" "100%", style "padding" "40px" ]
+            [ Html.form [ onSubmit (PostedMessage model.message) ]
+                [ input [ type_ "input", onInput TypedMessage, value model.message, style "width" "100%" ] []
+                ]
+            ]
+        , div []
+            [ div [ style "width" "35%", style "background-color" "white", style "margin" "auto", style "height" "100%", style "padding" "40px" ]
+                (model.messages
+                    |> List.reverse
+                    |> List.map (\message -> div [ style "padding" "5px" ] [ viewMessage message ])
+                )
+            ]
+        , br [] []
         ]
     }
